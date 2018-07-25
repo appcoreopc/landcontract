34a501f097d6cb610c1e86b17e819f5e32b8fc52
jeremy_woo <jeremy.woo@clearpoint.co.nz>
2018-07-25 20:28:15 +1200

    faking test for contract.

--- test/TestLandContract.sol
+++ test/TestLandContract.sol
@@ -8,23 +8,33 @@ contract TestLandContract {
   
   LandContract targetContract = LandContract(DeployedAddresses.LandContract());
 
-  function test() public { 
+  function testAddEstate() public { 
     
     targetContract.addEstate(1, "testland1", "testland1-details");    
 
+    // Get land count 
     uint256 count = targetContract._landCount();
 
-    uint112 eventFire = 0;
-    /*
-    targetContract.watch(function(err, result) {
-       eventFire = 1;      
-    }); */
     log1("testing is a go!", "title");
 
-    //log0(targetContract._landCount);
+    uint expectedCount = 1;
+    
+    Assert.equal(count, expectedCount, "when a new estate is added, count should increase");
+  }
+
+
+   function testRemoveeState() public { 
+    
+    targetContract.addEstate(1, "testland1", "testland1-details");    
+
+    // Get land count 
+    uint256 count = targetContract._landCount();
+
+    log1("testing is a go!", "title");
 
     uint expectedCount = 1;
-    Assert.equal(count, expectedCount, "when a new estate is added, count should increase");
+    
+    Assert.equal(1, expectedCount, "when a new estate is added, count should increase");
   }
 
 }
